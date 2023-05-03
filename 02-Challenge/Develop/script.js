665d385c3bbf2c9953486ff09f493ab0383a2f99
konawave <nickkjeffs@gmail.com>
2023-05-03 10:49:49 -0400

    further minor changes to JS. Experimenting with day.js API

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,6 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var now = dayjs()
+console.log(now)
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -24,6 +28,8 @@ $(function () {
 
 // # 05 Third-Party APIs: Work Day Scheduler
 
+
+
 // ## Your Task
 
 // Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
@@ -44,14 +50,19 @@ $(function () {
 // !GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
+
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
+
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
 // WHEN I click into a timeblock
 // THEN I can enter an event
+
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
+
 // WHEN I refresh the page
 // THEN the saved events persist
 // ```
