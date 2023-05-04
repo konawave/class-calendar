2ada96bcf119a7b7a39fd99d213ff926cada84ce
konawave <nickkjeffs@gmail.com>
2023-05-04 19:56:57 -0400

    experimenting with how to use the container id as a key for localStorage.setItem

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -8,23 +8,34 @@ $( document ).ready(function() {
   var currentDay = document.querySelector('#currentDay');
   currentDay.textContent = dateToday;
 
-  var saveBtn = document.querySelectorAll('.saveBtn');
-  var textBox = document.querySelectorAll('.description')
+  var saveBtn = document.querySelector('.saveBtn');
+  var textBox = document.querySelector('.description');
 
-  saveBtn.addEventListener('click', function() {
-    console.log(textBox.value);
-  } )
+  // saveBtn.addEventListener('click', function(event) {
+  //   localStorage
+  //   event.preventDefault;
 
-  $(function () {
+  //   localStorage.setItem('event', textBox.value);
+
+
+
+
+  // } )
+
+  // $(function () {
     
-    })
+  //   })
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+    $('.saveBtn').on('click', function() {
+      console.log(this.parentElement.id);
+      localStorage.this.parentElement.id(textBox.value);
 
+    })
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -70,8 +81,8 @@ $( document ).ready(function() {
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 
-// WHEN I click into a timeblock
-// THEN I can enter an event
+// !WHEN I click into a timeblock
+// !THEN I can enter an event
 
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
---
+++ psuedocode.js
@@ -0,0 +1,17 @@
+// Pseudocode:
+// Need the date to display at the top.
+    // The hour of the time must be useable to be compared to the ID of the textAreas so that they know when to switch from future to past to present
+
+// TextArea entries need to be saved to local storage, and added to the .textContent of the textArea when the respective button is clicked. 
+    //  preventDefault here
+
+// How to use the ID of the container of the button. 
+// Hint is 'this' in click listener, also DOM traversal. 
+
+// $('.saveBtn').on('click', function() {
+//     localStorage.set('event', textBox.value)
+//      for (i = 0; i < this.parentElemend.id[i]; i++)
+//     retreive = localStorage.get('event')
+//     textBox.append(textBox.value)
+//   })
+// this.parentElement.id
