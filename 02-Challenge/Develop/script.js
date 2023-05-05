3eb849370df2433f314ae8bb04e7f640b2c7991c
konawave <nickkjeffs@gmail.com>
2023-05-05 17:23:31 -0400

    savings before I experiment with removing class ids and re-appending them

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -4,6 +4,7 @@
 $( document ).ready(function() {
 
     $("#currentDay").text(dayjs().format("ddd, MMM DD, YYYY h:mm a"))
+    var hourId = dayjs().hour()
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -11,15 +12,6 @@ $( document ).ready(function() {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //  ! $('.saveBtn').on('click', function() {
-  //     console.log(this.parentElement.id);
-  //     const savedInput = $(this).siblings("textarea").val()
-  //     const timeInput = $(this).parent().attr("value")
-  //     // console.log(savedInput);
-  //     // console.log(timeInput);
-  //     localStorage.setItem('test', this.parentElement.id);
-
-  // })
   $(".saveBtn").on("click", function () {
     const savedInput = $(this).siblings("textarea").val()
     const timeInput = this.parentElement.id  
@@ -37,6 +29,11 @@ $( document ).ready(function() {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  $('timeBlock').each(function() {
+    if (Number(this.id) === hourId) {
+      
+    }
+  })
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
