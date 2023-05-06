fb7223aac0cc37d1fd619d7296a70a9d26b74a9d
konawave <nickkjeffs@gmail.com>
2023-05-06 12:01:27 -0400

    Finished with wet code! Next step: drying out code

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,21 +1,19 @@
 $(document).ready(function() {
-
+  // This displays the current date/time
   $("#currentDay").text(dayjs().format("ddd, MMM DD, YYYY h:mm a"))
   var hourId = dayjs().hour()
 
-  $(".saveBtn").on("click", function () {
+  // This saves the textArea inputs to local storage, with the id as the key and input as the value
+  $(".saveBtn").on("click", function(event) {
+    event.preventDefault();
     const savedInput = $(this).siblings("textarea").val()
     const timeInput = $(this).parent().attr('id');
-    console.log(savedInput)
-    console.log(timeInput)
     localStorage.setItem(timeInput, savedInput);
   });
   
-  for (let i = 9; i < 18; i++) {
-    localStorage.getItem(i);
-  }
-  
-  $('.time-block').each(function() {
+  // This changes the id between past, present, and future depending on what time it is. 
+  $('.time-block').each(function(event) {
+      event.preventDefault;
       if (Number(this.id) === hourId) {
         $(this).toggleClass('present');
       }
@@ -26,62 +24,9 @@ $(document).ready(function() {
         $(this).toggleClass('future');
 
       }
-  for (i = 9; i < 18; i++) {
-    
-  }
-
-
   });
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  //! TODO: Add code to display the current date in the header of the page.
-  });
-
-// # 05 Third-Party APIs: Work Day Scheduler
-
-
-
-// ## Your Task
-
-// Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-
-// You'll need to use the [Day.js](https://day.js.org/en/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
-
-// ## User Story
-
-// !```md
-// !AS AN employee with a busy schedule
-// !I WANT to add important events to a daily planner
-// !SO THAT I can manage my time effectively
-// !```
-
-// ## Acceptance Criteria
-
-// !```md
-// !GIVEN I am using a daily planner to create a schedule
-// !WHEN I open the planner
-// !THEN the current day is displayed at the top of the calendar
-
-// !WHEN I scroll down
-// !THEN I am presented with timeblocks for standard business hours
-
-// !WHEN I view the timeblocks for that day
-// !THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-
-// !WHEN I click into a timeblock
-// !THEN I can enter an event
-
-// !WHEN I click the save button for that timeblock
-// !THEN the text for that event is saved in local storage
-
-// WHEN I refresh the page
-// THEN the saved events persist
-// ```
-
-// The following animation demonstrates the application functionality:
-
-// <!-- @TODO: create ticket to review/update image) -->
-// [A user clicks on slots on the color-coded calendar and edits the events.](./Assets/05-third-party-apis-homework-demo.gif)
+  // This for-loop keeps text in their respective textArea upon page reloads. 
+  for (let i = 9; i < 18; i++) {
+    console.log($('#' + i).children('textArea'))
+    $('#' + i).children('textArea').text(localStorage.getItem(i));
+  }})
